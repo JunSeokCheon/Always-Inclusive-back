cb5a3b047c2f611ece3c44a82718ef8e0c2e24f7
JunSeokCheon <goosw775@naver.com>
2025-01-27 15:20:25 +0900

    ADD: 챗봇 프로필 변경 및 성별/나이 변경 로직 추가

--- AIO_backend/accounts/models.py
+++ AIO_backend/accounts/models.py
@@ -3,12 +3,14 @@ from django.db import models
 
 class User(AbstractUser):
     email = models.EmailField(unique=True)
-    # 성별, 나이대 필드를 추가 (선택 사항, blank=True, null=True 설정)
     gender = models.CharField(max_length=10, blank=True, null=True)     # "male", "female"
     age_group = models.CharField(max_length=10, blank=True, null=True)  # "10s", "20s", etc.
 
     nickname = models.CharField(max_length=50, blank=True, null=True)
     profile_image = models.ImageField(upload_to='profiles/', blank=True, null=True)
     
+    # 추가: 챗봇 이미지 필드
+    chatbot_image = models.ImageField(upload_to='chatbots/', blank=True, null=True)
+
     def __str__(self):
         return self.email
